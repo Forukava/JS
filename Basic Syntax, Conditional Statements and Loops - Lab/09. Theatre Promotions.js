f6e446133a9f6f70c8145e3b9f989d34cc8c4287
Forukava <47494978+Forukava@users.noreply.github.com>
2019-09-20 17:04:52 +0300

    Basic Syntax, Conditional Statements and Loops - Lab

--- Basic Syntax, Conditional Statements and Loops - Lab/09. Theatre Promotions.js
+++ Basic Syntax, Conditional Statements and Loops - Lab/09. Theatre Promotions.js
@@ -1,11 +1,24 @@
-function solve(lang){
-if(lang == 'England' || lang =='USA'){
-    console.log('English')
-}else if(lang == 'Spain' || lang == 'Mexico'|| lang == 'Argentina'){
-    console.log('Spanish')                          
+function solve(day, age){
+    if(day == 'Weekday' && 0<age>=18){
+    console.log('12$');
+}else if(day == 'Weekday' && 18<age>=64){
+    console.log('18$');
+}else if(day == 'Weekday' && 64<age>=122){
+    console.log('12$');
+}else if(day == 'Weekend' && 0<age>=18){
+console.log('15$');
+}else if(day =='Weekend' && 18<age>=64){
+console.log('20$');
+}else if(day == 'Weekend' && 64<age>=122){
+console.log('15$');
+}else if(day == 'Holiday' && 0<age>=18){
+console.log('5$');
+}else if(day == 'Holiday' && 18<age>=64){
+console.log('12$');
+}else if(day == 'Holiday' && 64<age>=122){
+console.log('10$');
 }else{
-    console.log('unknown')
+    console.log('Error!');
 }
 }
-
-solve('Germany')
+solve('Holiday', 15)
