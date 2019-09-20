0e0f54b97614fb9fe0a42298719e2a26c2b57f46
Forukava <47494978+Forukava@users.noreply.github.com>
2019-09-20 14:34:58 +0300

    Basic Syntax, Conditional Statements and Loops - Lab

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/02. Excellent grade.js
@@ -0,0 +1,9 @@
+function solve(a){
+if(a>5.50){
+console.log('Excellent');
+}else{
+    console.log('Not excellent')
+}
+
+}
+solve(5)
---
+++ Basic Syntax, Conditional Statements and Loops - Lab/03. Numbers from 1 to 5.js
@@ -0,0 +1,7 @@
+function solve(input){
+for (let index = 1; index < 6; index++) {
+    console.log(index)
+    
+}
+}
+solve()
