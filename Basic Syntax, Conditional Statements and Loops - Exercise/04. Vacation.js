7d555cb2cef13bba4198f9f37c7bb6d7cc2efe48
Forukava <47494978+Forukava@users.noreply.github.com>
2019-09-25 09:21:21 +0300

    Update 04. Vacation.js

--- Basic Syntax, Conditional Statements and Loops - Exercise/04. Vacation.js
+++ Basic Syntax, Conditional Statements and Loops - Exercise/04. Vacation.js
@@ -1,7 +1,7 @@
 function solve(num, type, day){
     if(type == "Students" && day == 'Friday'){
         let sum = num * 8.45
-        if (num<30){
+        if (num<29){
             console.log(`Total price: ${sum.toFixed(2)}`)
             }else{
             sum *= 0.85
@@ -10,7 +10,7 @@ function solve(num, type, day){
 }
     if(type == "Students" && day == 'Saturday'){
         let sum = num * 9.80
-        if (num<30){
+        if (num<29){
             console.log(`Total price: ${sum.toFixed(2)}`)
             }else{
             sum *= 0.85
@@ -19,7 +19,7 @@ function solve(num, type, day){
 }
 if(type == "Students" && day == 'Sunday'){
         let sum = num * 10.46;
-        if (num<30){
+        if (num<29){
             console.log(`Total price: ${sum.toFixed(2)}`)
             }else{
             sum *= 0.85
@@ -28,7 +28,7 @@ if(type == "Students" && day == 'Sunday'){
 }
 if(type == "Business" && day == 'Friday'){
             let sum = num * 10.90
-            if (num<100){
+            if (num<99){
                 console.log(`Total price: ${sum.toFixed(2)}`)
                 }else{
                 sum *= 0.90
@@ -37,7 +37,7 @@ if(type == "Business" && day == 'Friday'){
     }
     if(type == "Business" && day == 'Saturday'){
             let sum = num * 15.60
-            if (num<100){
+            if (num<99){
                 console.log(`Total price: ${sum.toFixed(2)}`)
                 }else{
                 sum *= 0.90
@@ -46,7 +46,7 @@ if(type == "Business" && day == 'Friday'){
     }
     if(type == "Business" && day == 'Sunday'){
             let sum = num * 16.00;
-            if (num<100){
+            if (num<99){
                 console.log(`Total price: ${sum.toFixed(2)}`)
                 }else{
                 sum *= 0.90
@@ -55,7 +55,7 @@ if(type == "Business" && day == 'Friday'){
     }
     if(type == "Regular" && day == 'Friday'){
         let sum = num * 15
-        if (num<=9 || num>=19){
+        if (num<9 || num>19){
             console.log(`Total price: ${sum.toFixed(2)}`)
             }else{
             sum *= 0.95
@@ -64,16 +64,16 @@ if(type == "Business" && day == 'Friday'){
 }
 if(type == "Regular" && day == 'Saturday'){
         let sum = num * 20
-        if (num<=9 || num>=19){
+        if (num<9 || num>19){
             console.log(`Total price: ${sum.toFixed(2)}`)
             }else{
             sum *= 0.95
             console.log(`Total price: ${sum.toFixed(2)}`)
     }
 }
-if(type == "Regular" && day == 'Sunday'){
+if(type === "Regular" && day === 'Sunday'){
         let sum = num * 22.50;
-        if (num<=9 || num>=19){
+        if (num<9 || num>19){
             console.log(`Total price: ${sum.toFixed(2)}`)
             }else{
             sum *= 0.95
