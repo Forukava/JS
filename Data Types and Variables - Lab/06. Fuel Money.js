efafb1486b0cbe804a613036cb3093af97fcf069
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-10-02 21:12:23 +0300

    Create 06. Fuel Money.js

---
+++ Data Types and Variables - Lab/06. Fuel Money.js
@@ -0,0 +1,7 @@
+function solve(dist, pass, price){
+let needFuel = (dist/100) * 7;
+needFuel += pass*0.100;
+let total = needFuel*price;
+console.log(`Need money for that trip is ${total} lv.`)
+}
+solve(260, 9, 2.49)
