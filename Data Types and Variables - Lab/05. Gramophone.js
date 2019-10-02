df9cfc84b13399a059a1e964cb38f52e6287a895
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-10-02 20:49:23 +0300

    Create 05. Gramophone.js

---
+++ Data Types and Variables - Lab/05. Gramophone.js
@@ -0,0 +1,6 @@
+function solve(band, album, song){
+let time = (band.length * album.length) * song.length /2
+let rotate = Math.ceil(time/2.5);
+console.log(`The plate was rotated ${rotate} times`)
+}
+solve('Black Sabbath', 'Paranoid', 'War Pigs')
