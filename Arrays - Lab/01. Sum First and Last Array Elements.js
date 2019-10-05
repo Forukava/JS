00a45b7df4564927d5de5ffff256955d1b9c1e9d
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-10-05 23:28:37 +0300

    Arrays - Lab

---
+++ Arrays - Lab/01. Sum First and Last Array Elements.js
@@ -0,0 +1,6 @@
+function solve(input){
+    let first = Number(input.shift());
+    let second = Number(input.pop())
+    console.log(first + second)
+}
+solve(['20', '30', '40'])
---
+++ Arrays - Lab/05. Sum Even Numbers.js
@@ -0,0 +1,9 @@
+function solve(num){
+    for (let i = 0; i < num.length; i++) {
+        num[i] = Number(arr[i]);
+        console.log(num)
+        let sum = 0;
+        
+    }
+}
+solve(['1','2','3','4','5','6'])
