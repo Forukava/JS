f104d104fcadc3c624fc4520efbd26a20af0c221
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-10-20 20:14:14 +0300

    arrays

--- Arrays - Lab/05. Sum Even Numbers.js
+++ Arrays - Lab/05. Sum Even Numbers.js
@@ -1,15 +1,13 @@
 function solve(arr){
     for (let i = 0; i < arr.length; i++) {
         arr[i] = Number(arr[i]);
+    }
+        let even = 0;
         
-        let sum = 0;
         for(let num of arr)
         if(num % 2 === 0){
-            sum += num
-            let sum = 0;
+            even += num  
         }
-        console.log(sum)
-        
+        console.log(even)
     }
-}
 solve(['1','2','3','4','5','6'])
--- Arrays - Lab/06. Even and Odd Subtraction .js
+++ Arrays - Lab/06. Even and Odd Subtraction .js
@@ -3,14 +3,12 @@ function solve(arr){
         arr[i] = Number(arr[i]);
     }
         let even = 0;
-        let odd = 0;
+    
         for(let num of arr)
         if(num % 2 === 0){
             even += num  
-        }else if(num % 2 != 0){
-            odd += num
         }
-        console.log(even - odd)
+        console.log(even)
     }
 
 solve([1,2,3,4,5,6])
