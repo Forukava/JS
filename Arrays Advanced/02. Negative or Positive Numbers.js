b8725c57d3cbe395dc9847e074f9efbbac7120d7
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-10-30 15:55:04 +0200

    ad

---
+++ Arrays Advanced/02. Negative or Positive Numbers.js
@@ -0,0 +1,14 @@
+function solve(arr){
+let da = [];
+for (let i = 0; i < arr.length; i++) {
+    if(i>0){
+        push.da
+        console.log(da)
+    }
+    
+}
+    
+} 
+
+
+solve([7, -2, 8, 9])
---
+++ Arrays Advanced/02.тест.js
@@ -0,0 +1,6 @@
+function solve(arr){
+const arr1 = arr.map(x => x - 2)
+console.log(arr1)
+} 
+solve([1,5,6,7])
+
