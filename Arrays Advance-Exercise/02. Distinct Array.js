c1c4a7c87d8ce36c306253bf2c636998560493af
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-11-25 19:42:02 +0200

    Create 02. Distinct Array.js

---
+++ Arrays Advance-Exercise/02. Distinct Array.js
@@ -0,0 +1,10 @@
+function dist(arr){
+    let arr1 = []
+for (let i = 0; i < arr.length; i++) {
+    if(!arr1.includes(arr[i])){
+        arr1.push(arr[i])
+}
+}
+console.log(arr1.join(' '))
+}
+dist([7, 8, 9, 7, 2, 3, 4, 1, 2])
