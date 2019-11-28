146ca64436dc9fee0d2d9f215f9c7d5313989cdd
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-11-28 16:44:16 +0200

    11

---
+++ Objects and Classes - Lab/01. Person Info.js
@@ -0,0 +1,12 @@
+function solve(firstName,lastName, age){
+    const obj = {firstName: firstName, lastName: lastName, age: age}
+
+    for(let prop in obj){
+        console.log(`${prop}: ${obj[prop]}`)
+    }
+    
+}
+solve("Peter", 
+"Pan",
+"20"
+)
