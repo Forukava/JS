6d0d9eb4951de1f2660df9f52050b6bbe957f94a
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-12-06 13:39:48 +0200

    Update 02. Towns.js

--- Objects and Classes - Lab/02. Towns.js
+++ Objects and Classes - Lab/02. Towns.js
@@ -1,11 +1,14 @@
 function solve(arr){
     let data = {}
         for (let row of arr) {
-            const [town, latitude, longitude] = row.split(' | ')
-            console.log(town, latitude, longitude)
+            const tokens = row.split(' | ')
+            const obj = {
+                town: tokens[0],
+                latitude: Number(tokens[1]).toFixed(2),
+                longitude: Number(tokens[2]).toFixed(2)
+            }
+            console.log(obj)
         }
-       
-    
 }
 solve(['Sofia | 42.696552 | 23.32601',
 'Beijing | 39.913818 | 116.363625']
