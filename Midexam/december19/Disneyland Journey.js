c0bab8ceee4cb1ae45ddbe9b0ee930ee7383fd89
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-12-11 18:36:43 +0200

    Update Disneyland Journey.js

--- Midexam/december19/Disneyland Journey.js
+++ Midexam/december19/Disneyland Journey.js
@@ -2,10 +2,27 @@ function disney(jour, month){
     let journeyCost = jour
     let needMonth = month
     
-if (needMonth % 3 === 0){
-    needMonth = journeyCost * 0.84
-}  
-let spend = journeyCost  * needMonth
-console.log(spend)
+    let sum = []
+    
+    for (let i = 1; i <= needMonth; i++) {
+        sum.push(i)
+        if(sum.includes(3)){
+            let three = (journeyCost * 2 * 0.25) * 0.16
+           totalThree = (journeyCost * 0.25) - three
+           var finalThree = totalThree + (journeyCost * 2 * 0.25) 
+        }
+        if(sum.includes(4)){
+            var four = (finalThree * 0.25) + journeyCost * 0.25
+        }
+        var finish = finalThree+four
+        if(finish > journeyCost){
+            let end = finish - journeyCost
+            console.log(`Bravo! You can go to Disneyland and you will have ${end}lv. for souvenirs.`)
+        }else{
+            let end1 = journeyCost - finish
+            console.log(`Sorry. You need ${end1}lv. more.`)
+        }
+    }
 }
-disney(1000, 4)
+
+disney(3600, 6)
