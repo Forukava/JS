4298d37262dc3f425fd57de765af2807134d06db
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-12-12 17:13:57 +0200

    Create 3. School Grades.js

---
+++ Associative Arrays - Lab/3. School Grades.js
@@ -0,0 +1,23 @@
+function solve(input){
+    let storage = new Map();
+
+    input.forEach(element => {
+        let token = element.split(' ')
+
+        if(storage.has(token[0])){
+            let oldQ = storage.get(token[0]);
+            storage.set(token[0], oldQ + Number(token[1]))
+        }else{
+            storage.set(token[0], Number(token[1]))
+        }
+    });
+    let entries = Array.from(storage.entries());
+
+    entries.forEach(element =>{
+        console.log(`${element[0]} -> ${element[1]}`)
+    })
+}
+solve(['tomatoes 10',
+'coffee 5',
+'olives 100',
+'coffee 40'])
