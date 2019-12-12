26a80277270ea6b37097a8a96a5cdb649d9752fa
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-12-12 17:11:47 +0200

    Update 02. Storage.js

--- Associative Arrays - Lab/02. Storage.js
+++ Associative Arrays - Lab/02. Storage.js
@@ -3,8 +3,19 @@ function solve(input){
 
     input.forEach(element => {
         let token = element.split(' ')
-        console.log(token)
+
+        if(storage.has(token[0])){
+            let oldQ = storage.get(token[0]);
+            storage.set(token[0], oldQ + Number(token[1]))
+        }else{
+            storage.set(token[0], Number(token[1]))
+        }
     });
+    let entries = Array.from(storage.entries());
+
+    entries.forEach(element =>{
+        console.log(`${element[0]} -> ${element[1]}`)
+    })
 }
 solve(['tomatoes 10',
 'coffee 5',
