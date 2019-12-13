01304c9011e9d264b05249ee21079dec7d32d9a8
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-12-13 12:59:53 +0200

    Update 3. School Grades.js

--- Associative Arrays - Lab/3. School Grades.js
+++ Associative Arrays - Lab/3. School Grades.js
@@ -3,7 +3,15 @@ function solve(input){
 
     input.forEach(element => {
         let token = element.split(' ');
-        console.log(token)
+        let key = token[0]
+        let value = token.map(Number);
+
+        if(journal.has(key)){
+            let oldValue = journal.get(key)
+            journal.set(key, oldValue.concat(value));
+        }else{
+            journal.set(key, value)
+        }
     });
 }
 solve([ 'Lilly 4 6 6 5', 'Tim 5 6', 'Tammy 2 4 3', 'Tim 6 6' ])
