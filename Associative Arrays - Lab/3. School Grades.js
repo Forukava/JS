a7a45eb9e84c0ac64123120a3c6106b482e50cdc
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-12-15 08:17:30 +0200

    Update 3. School Grades.js

--- Associative Arrays - Lab/3. School Grades.js
+++ Associative Arrays - Lab/3. School Grades.js
@@ -3,7 +3,7 @@ function solve(input){
 
     input.forEach(element => {
         let token = element.split(' ');
-        let key = token[0]
+        let key = token.shift()
         let value = token.map(Number);
 
         if(journal.has(key)){
@@ -12,6 +12,21 @@ function solve(input){
         }else{
             journal.set(key, value)
         }
-    });
-}
+    })
+            let sorted = Array.from(journal.entries())
+    .sort((a,b) => average(a[1]) - average(b[1]))
+        
+    sorted.forEach(element => {console.log(`${element[0]}: ${element[1].join(', ')}`);
+})
+function average(arr){
+    let sum = 0
+    arr.forEach(element => sum += element)
+    return sum/arr.length
+}   
+}    
+
+
+   
+
+
 solve([ 'Lilly 4 6 6 5', 'Tim 5 6', 'Tammy 2 4 3', 'Tim 6 6' ])
