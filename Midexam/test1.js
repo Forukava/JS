009b89ca9dd13f833b9b20f0b3cf3c3bcdb00cab
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-12-22 12:28:39 +0200

    1

--- Midexam/test1.js
+++ Midexam/test1.js
@@ -2,7 +2,7 @@ function solve(input){
     let loot = input.shift().split('|')
     
     for (const element of input) {
-        let fo = element.split(',')
+        let fo = element.split(' ')
             console.log(fo)
             
         
