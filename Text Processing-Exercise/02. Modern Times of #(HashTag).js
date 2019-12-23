2613e7372c33884a2d6208876a9514dabc5e772e
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2019-12-23 16:54:05 +0200

    Create 02. Modern Times of #(HashTag).js

---
+++ Text Processing-Exercise/02. Modern Times of #(HashTag).js
@@ -0,0 +1,10 @@
+function solve(input){
+let text = input.split(' ')
+.forEach(text => {
+    if(text.includes('#')){
+       
+    console.log(`${text.slice(1)}`)
+    }
+});
+}
+solve('Nowadays everyone uses # to tag a #special word in #socialMedia')
