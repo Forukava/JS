73fd4ecd34d3bd94e95ebfb38461a2de509dfa52
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-04 10:07:38 +0200

    1

--- Web/index.js
+++ Web/index.js
@@ -1,2 +1,9 @@
-const sayHello = require('./func.js')
-console.log(sayHello)
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function(req,res){
+    res.send('<h1>Hello</h1>')
+})
+
+app.listen(port, () => console.log(`Example app listening on port: ${port}`))
