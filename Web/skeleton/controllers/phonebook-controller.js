c635a306db83f7f3c73f70f505f30511d1baf4c6
Pavel Pavlov <47494978+Forukava@users.noreply.github.com>
2020-01-06 12:08:35 +0200

    1

---
+++ Web/skeleton/controllers/phonebook-controller.js
@@ -0,0 +1,10 @@
+const phonebook = require('../phonebook');
+
+module.exports = {
+  index: (req, res) => {
+    // TODO: load index page
+  },
+  addPhonebookPost:(req, res) => {
+    // TODO: add a phonebook object to the array
+  }
+}
